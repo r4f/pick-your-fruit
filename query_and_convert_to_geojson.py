f59affc95ba8f1df57378c99d94f1f42280ac1eb
r4f <3826906+r4f@users.noreply.github.com>
2025-05-20 22:12:10 +0200

    Add additional file to manually add further trees.

--- query_and_convert_to_geojson.py
+++ query_and_convert_to_geojson.py
@@ -5,6 +5,7 @@ from osmtogeojson import osmtogeojson
 overpass_url="https://overpass-api.de/api/interpreter"
 overpass_query_file="query_edible_trees.overpassql"
 geojson_result_file="deploy/fruit_trees.geojson"
+additional_geojson_features_file="additional_fruit_trees.geojson"
 
 with open(overpass_query_file, "r") as f:
     query_content = "".join(f.readlines())
@@ -13,6 +14,10 @@ with requests.post(url=overpass_url, data=query_content) as response:
     response_dict = json.loads(response.text)
 
 geojson_data = osmtogeojson.process_osm_json(response_dict)
+
+with open(additional_geojson_features_file, "r") as f:
+    additional_features_collection = json.load(f)
+geojson_data["features"].extend(additional_features_collection["features"])
 
 with open(geojson_result_file, "w") as f:
     json.dump(geojson_data, f, indent=2)
